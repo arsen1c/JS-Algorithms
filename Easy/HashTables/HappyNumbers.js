87b0670e935e084832cf418cecf169b884b84af9
Arsenic <vector2912@gmail.com>
2021-06-24 02:43:29 +0530

    fix: indentation

--- Easy/HashTables/HappyNumbers.js
+++ Easy/HashTables/HappyNumbers.js
@@ -1,7 +1,6 @@
 function sumAllNumbers(num) {
-	return num.toString().split('').reduce((a, b) => {
-		return parseInt(a) + parseInt(b);
-	}, 0)
+    return num.toString().split('').reduce((a, b) => {
+        return parseInt(a) + parseInt(b);
+    }, 0)
 }
-
 console.log(sumAllNumbers(1233));
