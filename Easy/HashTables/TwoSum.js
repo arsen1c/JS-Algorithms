8b8ab81431966dd7b0842d7d39b07e561f1e2b61
Arsenic <vector2912@gmail.com>
2021-06-24 02:45:08 +0530

    fix: indentation

--- Easy/HashTables/TwoSum.js
+++ Easy/HashTables/TwoSum.js
@@ -29,46 +29,46 @@
 */
 // Brute Force Way
 function towSum(arr, target) {
-    // Run through the array
-    for (let x = 0; x < arr.length; x++) {
-        // Check if current element has any compliment to sum up to target
-        for (let y = x + 1; y < arr.length; y++) {
-            // Check if x and y are not pointing to same element
-            // if the pointing to same element, continure
-            if (x === y) {
-                continue;
-            }
-            if (arr[x] + arr[y] === target) {
-                console.log([x, y]);
-                return [x, y];
-            }
-        };
-    };
+		// Run through the array
+		for (let x = 0; x < arr.length; x++) {
+				// Check if current element has any compliment to sum up to target
+				for (let y = x + 1; y < arr.length; y++) {
+						// Check if x and y are not pointing to same element
+						// if the pointing to same element, continure
+						if (x === y) {
+								continue;
+						}
+						if (arr[x] + arr[y] === target) {
+								console.log([x, y]);
+								return [x, y];
+						}
+				};
+		};
 };
 towSum([8, 8, 2, 2, 1], 10);
 // Two sum using Hash tables
 function twoSum1(arr, target) {
-    // Create an empty hash
-    const storage = new Map();
-    /* 
-     *	+ Basic Logic would be:
-     *			targetSum - currentElement = diff;
-     *				targetSum = diff + currentElement;
-     *			=> We have to look for the "difference" element in the array.
-     */
-    // Iterate through the array
-    for (let [index, element] of arr.entries()) {
-        // Check if the difference of "target" and "current element"
-        // Exists in the hash table. If it exists return the index of
-        // current element and the index of the element in hash table
-        // Else add fill the hash table
-        if (storage.has(target - element)) {
-            console.log([storage.get(target - element), index])
-            return [storage.get(target - element), index];
-        }
-        // Set current element and it's index in hash table
-        storage.set(element, index);
-    }
-    return null;
+		// Create an empty hash
+		const storage = new Map();
+		/* 
+		 *	+ Basic Logic would be:
+		 *			targetSum - currentElement = diff;
+		 *				targetSum = diff + currentElement;
+		 *			=> We have to look for the "difference" element in the array.
+		 */
+		// Iterate through the array
+		for (let [index, element] of arr.entries()) {
+				// Check if the difference of "target" and "current element"
+				// Exists in the hash table. If it exists return the index of
+				// current element and the index of the element in hash table
+				// Else add fill the hash table
+				if (storage.has(target - element)) {
+						console.log([storage.get(target - element), index])
+						return [storage.get(target - element), index];
+				}
+				// Set current element and it's index in hash table
+				storage.set(element, index);
+		}
+		return null;
 }
 twoSum1([4, 8, 5, 6], 2);
