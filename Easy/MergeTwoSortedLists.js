146990c89b0f8ece8d16e4e73ea4acea4caf1753
Arsenic <vector2912@gmail.com>
2021-06-24 11:51:56 +0530

    test

--- Easy/MergeTwoSortedLists.js
+++ Easy/MergeTwoSortedLists.js
@@ -1,20 +1,20 @@
 /*
-	LINK: https://leetcode.com/problems/merge-two-sorted-lists/
+  LINK: https://leetcode.com/problems/merge-two-sorted-lists/
 
-	Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing 
-	together the nodes of the first two lists.
+  Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing 
+  together the nodes of the first two lists.
 
-	![nodes_image](https://assets.leetcode.com/uploads/2020/10/03/merge_ex1.jpg)
+  ![nodes_image](https://assets.leetcode.com/uploads/2020/10/03/merge_ex1.jpg)
 
-	Example 1:
-		Input: l1 = [1,2,4], l2 = [1,3,4]
-		Output: [1,1,2,3,4,4]
+  Example 1:
+    Input: l1 = [1,2,4], l2 = [1,3,4]
+    Output: [1,1,2,3,4,4]
 
-	Example 2:
-		Input: l1 = [], l2 = []
-		Output: []
+  Example 2:
+    Input: l1 = [], l2 = []
+    Output: []
 
-	Example 3:
-		Input: l1 = [], l2 = [0]
-		Output: [0]
+  Example 3:
+    Input: l1 = [], l2 = [0]
+    Output: [0]
 */ 
